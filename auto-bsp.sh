8393120b9a7410e43e04e769a493563cc147678e
Mohamad Noor Alim bin Hussin <alimhussin2@gmail.com>
2018-04-23 13:36:32 +0800

    Create auto-bsp.sh

---
+++ auto-bsp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+COMMIT_ID=""
+SERVER_IP=""
+DUT_IP=""
+TARGET_MACHINE="intel-corei7-64"
+IMAGE_TESTED="core-image-sato"
+
+
+cd $HOME
+git clone git://git.yoctoproject.org/poky
+
+echo "INHERIT += \"testimage\"
+TEST_TARGET = \"simpleremote\"
+TEST_SERVER_IP = $SERVER_IP
+TEST_TARGET_IP = $DUT_IP
+MACHINE = \"$TARGET_MACHINE\"" >> $HOME/poky/build/conf/local.conf
+
+cd $HOME/poky
+#git checkout $COMMIT_ID
+source oe-init-build-env
+
+bitbake rpm busybox curl run-postinsts
+bitbake package-index
+
+ls $HOME/poky/build/tmp/deploy/images/$TARGET_MACHINE
+bitbake $IMAGE_TESTED -c testimage
+
+echo "Test $IMAGE_TESTED completed"
