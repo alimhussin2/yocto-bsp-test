d36f6768d19466829fe618ac786ca906142eae5d
Mohamad Noor Alim bin Hussin <alimhussin2@gmail.com>
2018-04-24 16:13:35 +0800

    Update auto-bsp.sh

--- auto-bsp.sh
+++ auto-bsp.sh
@@ -10,7 +10,7 @@ export http_proxy="http://192.168.122.2:9111"
 export https_proxy="http://192.168.122.2:9111"
 export ftp_proxy="http://192.168.122.2:9111"
 export socks_proxy="http://192.168.122.2:1080"
-
+export LC_ALL="en_US.UTF-8"
 echo '#!/bin/bash -xe
 exec socat STDIO SOCKS4:192.168.122.2:$1:$2,socksport=1080' > /usr/bin/git_proxy_command
 chmod 755 /usr/bin/git_proxy_command
