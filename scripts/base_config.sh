9d5bbba2222bb2a827a9f139139c02feefc1a9b1
Mohamad Noor Alim bin Hussin <alimhussin2@gmail.com>
2018-05-02 15:58:43 +0800

    Update base_config.sh

--- scripts/base_config.sh
+++ scripts/base_config.sh
@@ -4,23 +4,23 @@
 # git://git.yoctoproject.org/poky
 
 GIT_COMMIT_ID="9e3a9637b8f86f504e187b96cd0c98d8e1f651da"
-SERVER_IP="192.168.122.2"
-DUT_IP="192.168.122.100"
+SERVER_IP="localhost"
+DUT_IP="localhost"
 TARGET_MACHINE="intel-corei7-64"
 IMAGE_TESTED="core-image-sato"
 
 # create a file for git_proxy
 echo '#!/bin/bash -xe
-exec socat STDIO SOCKS4:192.168.122.2:$1:$2,socksport=1080' > /usr/bin/git_proxy_command
+exec socat STDIO SOCKS4:localhost:$1:$2,socksport=1080' > /usr/bin/git_proxy_command
 chmod 755 /usr/bin/git_proxy_command
 ls -lah /usr/bin/git_proxy_command
 cat /usr/bin/git_proxy_command
 
 # configure proxy
-export http_proxy="http://192.168.122.2:9111"
-export https_proxy="http://192.168.122.2:9111"
-export ftp_proxy="http://192.168.122.2:9111"
-export socks_proxy="http://192.168.122.2:1080"
+export http_proxy="http://localhost:80"
+export https_proxy="http://localhost:80"
+export ftp_proxy="http://localhost:80"
+export socks_proxy="http://localhost:80"
 export GIT_PROXY_COMMAND=/usr/bin/git_proxy_command
 export LC_ALL="en_US.UTF-8"
 
