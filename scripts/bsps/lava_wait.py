39c13af77ad3f61c5a011682a8316fe0f8ec897c
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-05-28 00:50:00 +0800

    lava_wait.py: change subprocess code.

--- scripts/bsps/lava_wait.py
+++ scripts/bsps/lava_wait.py
@@ -7,7 +7,7 @@ import sys
 
 print('LAVA now in idle mode...')
 #proc = subprocess.Popen(['python3', 'wait.py'])
-proc = subprocess.call(['lava-test-case', 'wait', '--shell', 'python3', 'wait.py'])
+proc = subprocess.call(['lava-test-case', 'wait', '--shell', 'python3', 'wait_child.py'])
 sys.stdout.flush()
 time.sleep(1)
 print('Idle mode end')
