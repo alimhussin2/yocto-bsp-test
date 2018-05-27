8d057065270f7e2aad3af0e5566622d9da7525c6
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-05-27 22:52:22 +0800

    lava_wait.py: run wait.py from subprocess of lava-test-case.

--- scripts/bsps/lava_wait.py
+++ scripts/bsps/lava_wait.py
@@ -6,7 +6,8 @@ import time
 import sys
 
 print('LAVA now in idle mode...')
-proc = subprocess.Popen(['python3', 'wait.py'])
+#proc = subprocess.Popen(['python3', 'wait.py'])
+proc = subprocess.call(['lava-test-case', 'wait', '--shell', 'python3', 'wait.py'])
 sys.stdout.flush()
 time.sleep(1)
 print('Idle mode end')
