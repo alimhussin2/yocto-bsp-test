88fc6faf9f62052bbd9db5d0ed09891c9a4a1786
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-05-27 22:00:06 +0800

    wait.py: wait now provide pid.

--- scripts/bsps/wait.py
+++ scripts/bsps/wait.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 
 import time
+import os
+import sys
 
-wait=3600
-print('wait for %s seconds' % (wait))
+pid = os.getpid()
+file_pid = 'wait.pid'
+timeout = 3600 # in seconds
+
+if os.path.exists(file_pid):
+    os.remove(file_pid)
+
+f = open(file_pid, 'w')
+f.write('%s' %(pid))
+f.close()
+
+print('IDLE  {:>6}: wait for %s seconds' .format(pid) %(timeout))
 time.sleep(wait)
+sys.stdout.flush()
