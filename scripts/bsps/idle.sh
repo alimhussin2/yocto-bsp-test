4afb7e4c0be66afccde7822a5fb8b77d6907cd6b
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-06-04 04:32:38 +0800

    idle.sh: change directory file for minnow.idle.done

--- scripts/bsps/idle.sh
+++ scripts/bsps/idle.sh
@@ -4,7 +4,7 @@ timeout=3600  # seconds
 
 run_timer() {
 local timer=0
-file_minnow_done="minnow.idle.done"
+file_minnow_done="$HOME/minnow.idle.done"
 if [[ -f $file_minnow_done ]]; then
     rm $file_minnow_done
 fi
@@ -12,7 +12,7 @@ fi
 while [[ $timer -lt $timeout ]];
 do
    if [[ -f $file_minnow_done ]]; then
-       echo 'Idle process was terminated...'
+       echo 'Idle process has been terminated...'
        break
    else
        sleep 1
