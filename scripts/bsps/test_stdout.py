fd2d5826b4aa5a1dfaa61cd292403ac65ac0611a
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-06-04 03:37:30 +0800

    remove test_stdout.py

--- scripts/bsps/test_stdout.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import os
-import subprocess
-flag = subprocess.Popen(['echo', "\$IDLE_FLAG"], stdout=subprocess.PIPE)
-idk = flag.communicate()
-print(idk)
-print(os.environ['IDLE_FLAG'])
-
