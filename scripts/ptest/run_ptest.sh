c7e4ffd73f50b90980a172b27dcffe642ede586d
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-09-21 14:45:02 +0800

    ptest: rearrange file for ptest
    Signed-off-by: Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>

---
+++ scripts/ptest/run_ptest.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+RESULTS_DIR="/home/root/ptest-results"
+NFS_DIR="/srv/data/LAVA/lava-job"
+
+which ptest-runner
+if [[ ! $0 -eq 0 ]]; then
+    echo "[  ERROR  ] Ptest-runner is not install"
+    exit 1
+fi
+
+job=`ls / | grep lava`
+JOB_ID=${job/lava-/}
+
+if [ ! -d $RESULTS_DIR ]; then
+    echo "[  INFO  ] Creating $RESULTS_DIR"
+    mkdir $RESULTS_DIR
+fi
+
+if [ ! -d "$NFS_DIR/ptest-results" ]; then
+    echo "[  INFO  ] Creating $NFS_DIR/ptest-results"
+    mkdir $NFS_DIR/ptest-results
+fi
+
+echo "[  INFO  ] ptest-runner started"
+ptest_list=`ptest-runner -l | awk -F ' ' '{ print $1 }'`
+for pt in $ptest_list;
+do
+    echo "ptest: $pt"
+    lava-test-case "ptest-runner-$pt" --shell "ptest-runner $pt 2>&1 | tee $RESULTS_DIR/$pt.log"
+    cp $RESULTS_DIR/$pt.log $NFS_DIR/$JOB_ID
+done
+
+echo "[  INFO  ] ptest-runner completed"
+
+
