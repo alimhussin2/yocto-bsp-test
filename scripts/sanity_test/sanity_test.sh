fa871bac6decb5b8267c849fa248ca3f9e60668c
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-09-24 10:47:31 +0800

    sanity_test.sh: Show message during execute sanity test
    Signed-off-by: Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>

--- scripts/sanity_test/sanity_test.sh
+++ scripts/sanity_test/sanity_test.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 
 RESULT="sanity_test.log"
 UPLOAD_DIR="/srv/data/LAVA/lava-job"
 
 echo "[  INFO  ] Sanity test started"
-# check kernel version
+echo "check kernel version" >> $RESULT
 uname -a >> $RESULT
 
-# check dmesg
+echo "check dmesg" >> $RESULT
 dmesg | grep -i -e 'error|fail' >> $RESULT
 
 # upload result
@@ -15,5 +15,6 @@ lava_job=`ls / | grep lava`
 lava_id=${lava_job/lava-/}
 UPLOAD_DIR=$UPLOAD_DIR/$lava_id
 cp $RESULT $UPLOAD_DIR
+echo "[  INFO  ] Upload result to $UPLOAD_DIR/$RESULT"
 echo "[  INFO  ] Sanity test completed"
 
