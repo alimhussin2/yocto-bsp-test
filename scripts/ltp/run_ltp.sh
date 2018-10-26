cfa39e8c3c02c1ccfda231116ee195b1baddd8e0
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2018-10-26 19:55:08 +0800

    run_ltp.sh: fixed runltp path
    Signed-off-by: Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>

--- scripts/ltp/run_ltp.sh
+++ scripts/ltp/run_ltp.sh
@@ -53,6 +53,6 @@ for t in ${testcases[@]}; do
     if [[ ! -z $ltp_path ]]; then
         runltp -p -f $t -l $UPLOAD_DIR/$t-`date +"%Y_%m_%d-%H_%M_%S"`.log
     else
-       /otp/ltp/runltp -p -f $t -l $UPLOAD_DIR/$t-`date +"%Y_%m_%d-%H_%M_%S"`.log
+       /opt/ltp/runltp -p -f $t -l $UPLOAD_DIR/$t-`date +"%Y_%m_%d-%H_%M_%S"`.log
    fi
 done
