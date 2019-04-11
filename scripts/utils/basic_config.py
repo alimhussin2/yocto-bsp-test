0f61f34cb7ebd5d25f7c46c9490d92882cf2b214
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2019-04-11 08:34:43 +0800

    basic_config: initial commit
    basic_config.py will contain basic configuration to configure
    device under test such as do mount nfs. More configuration
    will added soon.
    
    Signed-off-by: Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>

---
+++ scripts/utils/basic_config.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def do_mountnfs(nfsserver, src, dest):
+    cmd = 'mkdir -p %s; umount %s; mount %s:%s %s' % (dest, dest, nfsserver, src, dest)
+    p = subprocess.run(cmd, shell=True, timeout=10)
+    if p.returncode == 0:
+        print('[  OK  ] Successfully mount to NFS server')
+    else:
+        print('[  ERROR  ] Failed to mount NFS server')
