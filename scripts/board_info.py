8528c49f1dd569d6da55ad2a972e790a79080d84
Aaron Chan <31879367+lab-github@users.noreply.github.com>
2019-04-12 00:29:52 +0800

    Update board_info.py

--- scripts/board_info.py
+++ scripts/board_info.py
@@ -13,7 +13,7 @@ import re
 import os
 import sys
 from shutil import copyfile
-utilsdir=os.path.abspath(os.path.dirname(__file__))
+utilsdir=os.join.path(os.path.abspath(os.path.dirname(__file__)), "utils")
 putilsdir=os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 print("INFO: %s" % utilsdir)
 print("INFO: %s" % putilsdir)
