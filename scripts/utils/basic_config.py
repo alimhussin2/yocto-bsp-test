29dbf1438ed8f706e4cc96238aa23e7952a592bc
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2019-04-11 11:53:54 +0800

    basic_config: fixed code
    Signed-off-by: Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>

--- scripts/utils/basic_config.py
+++ scripts/utils/basic_config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import subprocess
+
 def do_mountnfs(nfsserver, src, dest):
     cmd = 'mkdir -p %s; umount %s; mount %s:%s %s' % (dest, dest, nfsserver, src, dest)
     p = subprocess.run(cmd, shell=True, timeout=10)
