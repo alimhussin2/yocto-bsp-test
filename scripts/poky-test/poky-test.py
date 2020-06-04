2768491e0a4e4817ee09515d6daa9e3b8daae79c
Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>
2020-06-04 11:42:57 +0800

    poky-test.py: Fix search OS name
    Signed-off-by: Mohamad Noor Alim Hussin <mohamad.noor.alim.hussin@intel.com>

--- scripts/poky-test/poky-test.py
+++ scripts/poky-test/poky-test.py
@@ -3,15 +3,18 @@
 import sys
 import os
 import subprocess
+import re
 
 def check_os_version(os_name):
     f_os = "/etc/issue"
-    output = None
 
     if os.path.isfile(f_os):
-        cmd = "cat %s | grep %s" % (f_os, os_name)
-        output = subprocess.check_output(cmd, shell=True).decode().rstrip('\n')
-    return output
+        with open(f_os) as f:
+            value = re.findall(r'^.+(?=\\n)', f.readline())[0]
+            if os_name in value:
+                return {"match": True, "value": value}
+            else:
+                return {"match": False, "value": value}
 
 def check_kernel():
     cmd = "uname -a"
@@ -19,14 +22,19 @@ def check_kernel():
     return kernel
 
 if __name__ == "__main__":
-    NFSSERVER = sys.argv[1]
+    try:
+        NFSSERVER = sys.argv[1]
+    except:
+        NFSSERVER = ""
+    print("NFSSERVER = %s" % NFSSERVER)
     os_name = "Poky"
     os_version = check_os_version(os_name)
-    if not os_version is None:
+
+    if os_version["match"] is True:
         script_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         board_config = os.path.join(script_dir, "board_config")
         idle_job = os.path.join(script_dir, "lava_wait.py")
-        print(os_version)
+        print(os_version["value"])
         print("Kernel: %s" % check_kernel())
         cmds = ["bash %s %s" % (board_config, NFSSERVER), "python3 %s" % idle_job]
  
@@ -35,6 +43,6 @@ if __name__ == "__main__":
             subprocess.run(cmd, shell=True)
 
     else:
-        print('os version is %s' % os_version)
+        print('os version is %s' % os_version["value"])
         print(check_kernel())
 
